7617ceabb7d9a2776759c77d22a92e150d1048a4
Thomas Baumann <43672505+thobauma@users.noreply.github.com>
2021-12-21 13:28:54 +0100

    new parameter classifier, if true returns dino, linear classifier, if false only returns dino

--- src/model/dino_model.py
+++ src/model/dino_model.py
@@ -3,7 +3,7 @@ from dino.eval_linear import LinearClassifier
 from dino import utils
 
 
-def get_dino(model_name='vit_small', patch_size=16,n_last_blocks=4, avgpool_patchtokens=False, device='cuda', pretrained_classifier = True, num_labels=1000):
+def get_dino(model_name='vit_small', patch_size=16,n_last_blocks=4, avgpool_patchtokens=False, device='cuda',classifier = True, pretrained_classifier = True, num_labels=1000):
     if model_name in vits.__dict__.keys():
         model = vits.__dict__[model_name](patch_size=patch_size, num_classes=0)
         embed_dim = model.embed_dim * (n_last_blocks + int(avgpool_patchtokens))
@@ -14,6 +14,8 @@ def get_dino(model_name='vit_small', patch_size=16,n_last_blocks=4, avgpool_patc
     model.cuda()
     model.eval()
     print(f"Model {model_name} built.")
+    if classifier != True:
+        return model
     
     linear_classifier = LinearClassifier(embed_dim, num_labels=num_labels)
     linear_classifier = linear_classifier.cuda()
@@ -21,3 +23,4 @@ def get_dino(model_name='vit_small', patch_size=16,n_last_blocks=4, avgpool_patc
         utils.load_pretrained_linear_weights(linear_classifier, model_name, patch_size)
 #    linear_classifier = nn.parallel.DistributedDataParallel(linear_classifier, device_ids=[arch.gpu])
     return model, linear_classifier
+
