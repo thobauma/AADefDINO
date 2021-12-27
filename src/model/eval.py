3a95b3665bfe4924d227060fe95442dc19d19028
Thomas Baumann <43672505+thobauma@users.noreply.github.com>
2021-12-27 15:08:10 +0100

    cleanup

--- src/model/eval.py
+++ src/model/eval.py
@@ -1,13 +1,15 @@
 import torch
 from torch import nn
+import pandas as pd
 import torch.distributed as dist
 import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import tqdm
 from dino import utils
 
 @torch.no_grad()
-def validate_network(val_loader, model, linear_classifier, n=4, avgpool=False):
+def validate_network_dino(val_loader, model, linear_classifier, n=4, avgpool=False):
     linear_classifier.eval()
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
@@ -15,7 +17,10 @@ def validate_network(val_loader, model, linear_classifier, n=4, avgpool=False):
         num_labels = linear_classifier.num_labels
     else:
         num_labels = linear_classifier.module.num_labels
-    for inp, target in metric_logger.log_every(val_loader, 20, header):
+        
+        
+    predicted_labels, true_labels, sample_names = [], [], []
+    for inp, target, sample_name in metric_logger.log_every(val_loader, 20, header):
         # move to gpu
         inp = inp.cuda(non_blocking=True)
         target = target.cuda(non_blocking=True)
@@ -32,7 +37,11 @@ def validate_network(val_loader, model, linear_classifier, n=4, avgpool=False):
                 output = model(inp)
         output = linear_classifier(output)
         loss = nn.CrossEntropyLoss()(output, target)
-
+        
+        predicted_labels.extend(output.tolist())
+        true_labels.extend(target.tolist())
+        sample_names.extend(sample_name)
+        
         if num_labels >= 5:
             acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
         else:
@@ -49,4 +58,5 @@ def validate_network(val_loader, model, linear_classifier, n=4, avgpool=False):
     else:
         print('* Acc@1 {top1.global_avg:.3f} loss {losses.global_avg:.3f}'
           .format(top1=metric_logger.acc1, losses=metric_logger.loss))
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}, pd.DataFrame({'file': sample_names,'predicted_labels': predicted_labels, 'true_labels': true_labels})
+
